dc20c96198446e5060bec22113800de86c4c7ee1
songshuangshuang <1024298102@qq.com>
2021-07-30 13:34:53 +0800

    build: 配置修改

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  * @version: V1.0.0
  * @Author: Shuangshuang Song
  * @Date: 2021-07-29 15:19:09
- * @LastEditTime: 2021-07-29 16:49:42
+ * @LastEditTime: 2021-07-30 11:35:14
  * @LastEditors: Shuangshuang Song
  */
 
-export { isString, isArray } from './utils';
+export { isString, isArray } from './src/utils';
