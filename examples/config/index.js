3c40149462babc3574d7b89409685f1a63d725a3
宋爽爽 <songshuangshuang@pin-dao.cn>
2022-02-22 17:33:20 +0800

    del: 去掉个人服务地址

--- examples/config/index.js
+++ examples/config/index.js
@@ -2,7 +2,7 @@
  * @Description: 配置编译环境和线上环境之间的切换
  * @Author: Shuangshuang Song
  * @Date: 2021-12-16 15:12:59
- * @LastEditTime: 2022-02-22 16:55:46
+ * @LastEditTime: 2022-02-22 17:32:00
  * @LastEditors: Song Shuang Shuang
  */
 
@@ -11,7 +11,7 @@ const Host = new Map();
 
 // 本地开发环境
 Host.set('development', {
-  baseUrl: 'https://scm-test.pin-dao.cn',
+  baseUrl: '', // 服务地址
   inventoryApi: '/distribution-center-inventory/v1', // 大仓api
 });
 
